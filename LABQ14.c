88d572f0994ce3f1749dfb19f14ebd5209aede48
hochang22 <hyo@naver.com>
2024-07-11 12:33:08 +0900

    NEwfile

--- LABQ14.c
+++ LABQ14.c
@@ -10,8 +10,8 @@ int main(){
      for(int A = 1; A <= 500; A++){
 
         for(int B = 1; B <= A; B++){
-            if( (A * A) > N * (B * B)) count++;
-                if( (A * A) <= N * (B * B)) break;
+            if( (A * A) - (B * B) == N) count++;
+                if( (A * A) - (B * B) < N) break;
 									      
         }
      }
